2f2b063c043f7abcb4e0a2958ba2867d7c98da60
Asmoniel <danishi@disroot.org>
2023-08-03 17:51:05 -0400

    add build file

---
+++ 8086/build.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+if "%~1"=="" (
+	echo You need to pass a parameter >&2
+	echo Example: build.bat 2
+	exit /b
+)
+
+set root=%cd%
+
+set WARNINGS=/W3 /WX /wd4996
+set NAME=%1
+
+pushd build
+cl %root%\%NAME%.c /nologo %WARNINGS% /Fe%NAME%.exe /MD
+popd
